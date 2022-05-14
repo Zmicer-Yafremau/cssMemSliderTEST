c0d24a26d52b8bb07beda7a42739310bf0755332
Zmicer-Yafremau <dmitry.mickevich@gmail.com>
2022-05-15 00:42:35 +0300

    fix: edit code style

--- cssMemSlider/sripts.js
+++ cssMemSlider/sripts.js
@@ -1,64 +1,55 @@
-/*const TEXT = ['First slide','Second slide','Third slide','Fourth slide'];*/
+
 const IMAGE = [`url("./assets/slide0.png")`,
-`url("./assets/slide1.png")`, 
-  `url("./assets/slide2.png")`,
-  `url("././assets/slide3.png")`];
+               `url("./assets/slide1.png")`, 
+               `url("./assets/slide2.png")`,
+              `url("././assets/slide3.png")`];
 
 let current = 0;
 let countMerg =0;
 const SLIDES = document.getElementById("slides");
-
 const CIRCLE = document.querySelectorAll(".circle");
 const CIRCLES = document.querySelectorAll(".circles");
-
-
-CIRCLES.forEach( (value) => {
+CIRCLES.forEach( ( value ) => {
   value.addEventListener("mouseover",
-                       () => {value.lastChild.style.borderColor = "white";
-if(!value.lastChild.classList.contains("active")){
+                       () => { value.lastChild.style.borderColor = "white";
+if ( !value.lastChild.classList.contains( "active" ) ){
  value.lastChild.style.backgroundColor = "white";
-
 }
                              })
 });
 
 
-CIRCLES.forEach( (value) => {
-  value.addEventListener("mouseout",
+CIRCLES.forEach( ( value ) => {
+value.addEventListener("mouseout",
                        () => { 
-   
-    value.lastChild.style.borderColor = "";
-  
-if(!value.lastChild.classList.contains("active")){
- 
- value.lastChild.style.backgroundColor = ""
+value.lastChild.style.borderColor = "";
+if( !value.lastChild.classList.contains( "active" ) ){
+  value.lastChild.style.backgroundColor = ""
 }}
                         )
   
 });
 
-CIRCLES.forEach( (value) => {
-  value.addEventListener("click",
+CIRCLES.forEach( ( value ) => {
+value.addEventListener( "click",
                        () => {
 
-if(value.lastChild.classList.contains("active")){
+if ( value.lastChild.classList.contains( "active" ) ){
  
-      document.querySelector('.memes').style.backgroundImage = "";  
-    /*document.querySelector('span').innerHTML = TEXT[ +value.lastChild.className.match(/[012345]/)[0] ];*/
-  current = +value.lastChild.className.match(/[012345]/)[0];
+   document.querySelector('.memes').style.backgroundImage = "";  
+  current = +value.lastChild.className.match( /[012345]/ )[0];
 }
   else {
     
-     document.querySelector('.active').classList.toggle("active");
-  value.lastChild.classList.toggle("active");
-     value.lastChild.style.backgroundColor = ""
-/*document.querySelector('span').innerHTML = TEXT[ +value.lastChild.className.match(/[012345]/)[0] ];*/
-  document.querySelector('.memes').style.backgroundImage = IMAGE[ +value.lastChild.className.match(/[012345]/)[0] ];
-    
-  current = +value.lastChild.className.match(/[012345]/)[0];
-    
-     countMerg = -25*current;
-      SLIDES.style.marginLeft=countMerg+"rem";
+    document.querySelector( '.active' ).classList.toggle( "active" );
+    value.lastChild.classList.toggle( "active" );
+    value.lastChild.style.backgroundColor = ""
+    document.querySelector( '.memes' ).style.backgroundImage = IMAGE[ +value.lastChild
+                                                                            .className
+                                                                            .match( /[012345]/ )[0] ]; 
+    current = +value.lastChild.className.match( /[012345]/ )[0];
+    countMerg = -25*current;
+    SLIDES.style.marginLeft=countMerg+"rem";  
 
   }
   }
